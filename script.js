f0ac1e01ee2fa08bfbc4ce301be113fc186f66f8
Hugo <wheredidhugo@protonmail.com>
2022-06-01 23:10:24 +0200

    chore: init

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".main-title", {
+  strings: ["4404 ^5000", "4404 = 2202 x 2"],
+  typeSpeed: 200,
+  smartBackspace: true,
+});
+
+var typed2 = new Typed(".main-description", {
+  strings: ["^1500No projects found (at the moment)"],
+  typeSpeed: 75,
+});
