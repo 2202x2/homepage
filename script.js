17b2f7a0f35fdcac46c5d2486ac3165cec594648
Hugo <wheredidhugo@protonmail.com>
2022-06-01 23:39:11 +0200

    feat: added loop to title + backSpeed same as typeSpeed

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+var speed = 200;
+
 var typed = new Typed(".main-title", {
-  strings: ["4404 ^5000", "4404 = 2202 x 2"],
-  typeSpeed: 200,
+  strings: ["4404 ^5000", "4404 = 2202 x 2^5000", "4404 = 2202 * 2"],
+  typeSpeed: speed,
+  backSpeed: speed,
   smartBackspace: true,
+  loop: true,
+  loopCount: Infinity,
 });
 
 var typed2 = new Typed(".main-description", {
